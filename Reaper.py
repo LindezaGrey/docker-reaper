1bd7330a5b352633e4ae045cc70e4370717e855d
Unknown <andyvziegler@web.de>
2018-11-05 20:08:05 +0100

    Finalizing

--- Reaper.py
+++ Reaper.py
@@ -5,10 +5,10 @@ import time
 client = docker.from_env()
 print("Let the killing begin :]")
 while True:
-        print("ein neuer zyklus beginnt")
+        # print("ein neuer zyklus beginnt")
         # check all containers on the host
         for container in client.containers.list():
-                print("scanning...found:", container.name)
+                # print("scanning...found:", container.name)
                 if 'DeathDate' in container.attrs['Config']['Labels']:
                         # get the deathdate label of the containers in order to shut them down
                         DeathDate = datetime.strptime(container.attrs['Config']['Labels']['DeathDate'], '%Y-%m-%d %H:%M:%S')
